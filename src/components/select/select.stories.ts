f4b047bf6455eee5a9f260c13f83e0a05547edee
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-11-24 18:39:13 +0800

    Build Select component and stories

---
+++ src/components/select/select.stories.ts
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Select } from './select'
+
+const meta: Meta<typeof Select> = {
+  component: Select,
+  title: 'Select',
+}
+
+export default meta
+type Story = StoryObj<typeof Select>
+
+export const Default: Story = {
+  args: {
+    label: 'Options',
+    items: [
+      {
+        label: 'Basic Pack',
+        description: 'Free',
+      },
+      {
+        label: 'Pro Pack',
+        description: '$9.99',
+      },
+      {
+        label: 'Ultimate Pack',
+        description: '$19.99',
+      },
+    ],
+  },
+}
---
+++ src/components/select/select.tsx
@@ -0,0 +1,63 @@
+import {
+  Select as AriaSelect,
+  Button,
+  ListBox,
+  ListBoxItem,
+  ListBoxItemProps,
+  Popover,
+  SelectProps,
+  SelectValue,
+  Text
+} from 'react-aria-components'
+import IconArrowDown from '../../assets/sign-up/icon-arrow-down.svg?react'
+import IconCheck from '../../assets/sign-up/icon-check.svg?react'
+
+import './style.css'
+
+interface Option {
+  label: string
+  description?: string
+}
+
+interface Props extends SelectProps {
+  label: string
+  items: Option[]
+}
+
+export const Select = ({ label, items, ...props }: Props) => {
+
+  return (
+    <div className="select-container">
+      <AriaSelect aria-label={label} {...props}>
+        <Button>
+          <SelectValue />
+          <IconArrowDown className='dropdown-arrow' aria-hidden="true" />
+        </Button>
+        <Popover offset={24}>
+          <ListBox items={items}>
+            {item => <Option id={item.label} label={item.label} description={item.description} />}
+          </ListBox>
+        </Popover>
+      </AriaSelect>
+    </div>
+  )
+}
+
+interface OptionProps extends ListBoxItemProps {
+  label: string
+  description?: string
+}
+
+export const Option = ({ label, description, ...props }: OptionProps) => {
+  return (
+    <ListBoxItem textValue={label} {...props}>
+      {({ isSelected }) => (
+        <>
+          <Text slot='label'>{label}</Text>
+          {description && <Text slot='description'>{description}</Text>}
+          {isSelected && <IconCheck className='icon-check' aria-hidden="true" />}
+        </>
+      )}
+    </ListBoxItem>
+  )
+}
